31370cfbecf16f1e38a646a3491142f7944b7293
Gerrit Kaiser <gerrit@gerritkaiser.de>
2010-04-21 21:35:24 +0100

    carefully refactored drag_order controller code for readability
    - comments=>method names
    - no actual logic changes
    - extracted a finder into Page model
    - use #save! instead of unchecked #save

--- lib/drag_order/page_controller_extensions.rb
+++ lib/drag_order/page_controller_extensions.rb
@@ -1,17 +1,19 @@
 module DragOrder::PageControllerExtensions
 
   def move_to
-    @page = Page.find(params[:id])        # the moving page record
-    @rel = Page.find(params[:rel])        # the page record of the target of the move
-    @loc = params[:pos].to_i              # the moving page current location
-    @copy = params[:copy].to_i > 0        # whether this is copied or not
+    @page = Page.find(params[:id])
+    @old_parent = @page.parent
+    @loc = params[:pos].to_i
+    
+    remove_page_from_old_position unless copying?
+    
+    @rel = Page.find(params[:rel])
     
     # check first to see if there are any new, incoming pages with nil position values.
     # mimic the way Radiant orders the objects on index page with new positions
-    
     all_new_pages = Page.find_all_by_parent_id( @page.parent.id, :conditions => ["position is null"] )
     if(all_new_pages.size > 0)
-      all_pages = Page. find_all_by_parent_id( @page.parent.id, :order => ["position ASC"] )
+      all_pages = Page.find_all_by_parent_id( @page.parent.id, :order => ["position ASC"] )
       i = 1
       all_pages.each do |p|
         p.position = i
@@ -21,101 +23,109 @@ module DragOrder::PageControllerExtensions
       @page = Page.find(params[:id])
       @rel = Page.find(params[:rel])
     end
-
     
-    # re-establish the incoming page object with its new position (otherwise is still nil)
+    make_room_for_page if @loc != 2
     
-     if !@copy
-        # Remove the page from its old position
-        @old_siblings = Page.find_all_by_parent_id( @page.parent.id, :conditions => [ 'position > ?', @page.position ] )
-        @old_siblings.each do |s|
-          s.position -= 1
-          s.save
-        end
-      end
-
-      @rel.reload
-      if @loc != 2
-        # Make room for the page
-        @new_siblings = Page.find_all_by_parent_id( @rel.parent.id, :conditions => [ 'position >= ?', (@rel.position.to_i + @loc).to_s ] )
-        @new_siblings.each do |s|
-          if s.id != @page.id || @copy
-            s.position += 1
-            s.save
-          end
-        end
-      end
-
-      if @copy
-        # Save reference to parts
-        orig_parts = @page.parts
-        # Create copy of page
-        @page = @page.clone
-      end
-
-      # Put the page
-      @rel.reload
-      old_parent = @page.parent
-      if @loc != 2
-        @page.parent = @rel.parent
-        @page.position = @rel.position.to_i + (@loc == 1 ? 1 : -1)
-      else
-        @page.parent = @rel
-        @page.position = 1
-      end
-
-      if @copy || @page.parent != old_parent # @page.parent.changed? always gives false...
-        # Check for slug duplication
-        check_slug = @page.slug.sub /-copy-?[0-9]*$/, ""
-        count = 0
-        duplicates = Page.find_all_by_parent_id( @loc == 2 ? @rel.id : @rel.parent.id, :conditions => [ "slug LIKE '?%%'", check_slug ] )
-        duplicates.each do |d|
-          m = d.slug.match("^#{check_slug}(-copy-?([0-9]*))?$")
-          if !m.nil?
-            if !(m[2].nil? || m[2] == "")
-              nc = m[2].to_i + 1
-            elsif m[1]
-              nc = 2
-            else
-              nc = 1
-            end
-            count = nc if nc > count
-          end
-        end
-        if count > 0
-          # Remove old copy counters
-          re = / - COPY ?[0-9]*$/
-          @page.title.sub! re, ""
-          @page.breadcrumb.sub! re, ""
-          # Add new copy counters
-          @page.slug = check_slug + "-copy" + (count > 1 ? "-" + count.to_s : "")
-          @page.title += " - COPY" + (count > 1 ? " " + count.to_s : "")
-          @page.breadcrumb += " - COPY" + (count > 1 ? " " + count.to_s : "")
-        end
-      end
-
-      # Save the page
-      @page.save
-
-      if @copy
-        # Create copy of parts
-        orig_parts.each do |op|
-          @page.parts.create({
-            :name => op.name,
-            :filter_id => op.filter_id,
-            :content => op.content
-          })
-        end
-      end
+    if copying?
+      @orig_parts = @page.parts
+      @page = @page.clone
+    end
     
-    clear_cache!
+    @rel.reload
     
-    # Redirect back to the admin pages page
-    request.env["HTTP_REFERER"] ? redirect_to(:back) : redirect_to(admin_page_url)
+    put_page
+    
+    solve_slug_conflicts if copying? || new_parent_different?
+    
+    @page.save!
+    
+    create_copy_of_parts if copying?
+    
+    clear_cache
+    redirect_back_or_to_admin_pages_page
   end
   
 private
-  def clear_cache!
+  def copying?
+    params[:copy].to_i > 0
+  end
+  
+  def remove_page_from_old_position
+    @page.following_siblings.each do |sibling|
+      sibling.position -= 1
+      sibling.save!
+    end
+  end
+  
+  def put_page
+    if @loc != 2
+      @page.parent = @rel.parent
+      @page.position = @rel.position.to_i + (@loc == 1 ? 1 : -1)
+    else
+      @page.parent = @rel
+      @page.position = 1
+    end
+  end
+  
+  def make_room_for_page
+    new_siblings = Page.find_all_by_parent_id(@rel.parent_id, :conditions => [ 'position >= ?', @rel.position + @loc ])
+    new_siblings.each do |sibling|
+      if sibling != @page || copying?
+        sibling.position += 1
+        sibling.save!
+      end
+    end
+  end
+  
+  def new_parent_different?
+    # @page.parent.changed? always gives false...
+    @page.parent != @old_parent
+  end
+  
+  def solve_slug_conflicts
+    check_slug = @page.slug.sub(/-copy-?[0-9]*$/, "")
+    count = 0
+    duplicates = Page.find_all_by_parent_id( @loc == 2 ? @rel.id : @rel.parent.id, :conditions => [ "slug LIKE '?%%'", check_slug ] )
+    duplicates.each do |d|
+      m = d.slug.match("^#{check_slug}(-copy-?([0-9]*))?$")
+      if !m.nil?
+        if !(m[2].nil? || m[2] == "")
+          nc = m[2].to_i + 1
+        elsif m[1]
+          nc = 2
+        else
+          nc = 1
+        end
+        count = nc if nc > count
+      end
+    end
+    if count > 0
+      # Remove old copy counters
+      re = / - COPY ?[0-9]*$/
+      @page.title.sub! re, ""
+      @page.breadcrumb.sub! re, ""
+      # Add new copy counters
+      @page.slug = check_slug + "-copy" + (count > 1 ? "-" + count.to_s : "")
+      @page.title += " - COPY" + (count > 1 ? " " + count.to_s : "")
+      @page.breadcrumb += " - COPY" + (count > 1 ? " " + count.to_s : "")
+    end
+  end
+  
+  def create_copy_of_parts
+    @orig_parts.each do |op|
+      @page.parts.create({
+        :name => op.name,
+        :filter_id => op.filter_id,
+        :content => op.content
+      })
+    end
+  end
+
+  def redirect_back_or_to_admin_pages_page
+    request.env["HTTP_REFERER"] ? redirect_to(:back) : redirect_to(admin_page_url)
+  end
+
+  def clear_cache
     if defined? ResponseCache == 'constant'
       ResponseCache.instance.clear
     else
--- lib/drag_order/page_extensions.rb
+++ lib/drag_order/page_extensions.rb
@@ -2,10 +2,28 @@ module DragOrder::PageExtensions
   def self.included(base)
     base.class_eval {
       self.reflections[:children].options[:order] = "position ASC"
+
+      before_validation_on_create :set_initial_position
     }
     
     if defined?(Page::NONDRAFT_FIELDS)
       Page::NONDRAFT_FIELDS << 'position'
+    end
+  end
+  
+  
+  def following_siblings
+    self.class.find_all_by_parent_id(parent_id, :conditions => [ 'position > ?', position ] )
+  end
+
+private
+  def set_initial_position
+    self.position ||= begin
+      if last_sibling = Page.find_by_parent_id(parent_id, :order => [ "position DESC" ])
+        last_sibling.position + 1
+      else
+        0
+      end
     end
   end
 end
